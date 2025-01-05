e47296a2edac8634a3a899e8498c91141fd30c98
Your Name <your.email@example.com>
2025-01-05 11:03:36 -0500

    Update repository URLs to GrassAppSitev3

--- grassapp/vite.config.js
+++ grassapp/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/GrassAppSitev2/',
+  base: '/GrassAppSitev3/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
