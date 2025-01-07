c16ec57d37a2d845e91f35e1d362e20035b513b3
WaterVibes <your-email@example.com>
2025-01-07 05:31:29 -0500

    Update Vite config base URL for custom domain

--- grassapp/vite.config.js
+++ grassapp/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/GrassAppSitev3/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
